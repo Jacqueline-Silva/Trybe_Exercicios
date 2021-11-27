3b2e66e187ec47f149d4eccda00eee0cf093da1c
Jacqueline Silva <jacque.sx@hotmail.com>
2021-11-26 23:28:07 -0300

    exercicios b4-1 - incompleto

---
+++ M1_Fundamentos/bloco-04_introducao-a-javascript-e-logica-da-programacao/dia-01_variaveis/script-exercicios-aula4-1.js
@@ -0,0 +1,39 @@
+// 1- difina o valor de A e B e faça operações aritmeticas //
+const a = 25;
+const b = 10;
+const c = 96;
+
+console.log(adicao = a + b);
+console.log(subtracao = a - b);
+console.log(multiplicacao = a * b);
+console.log(divisao = a / b);
+console.log(modulo = a % b);
+
+// 2- se A for maior que B então retorna A //
+
+if (a > b) {
+    console.log(a)
+} else {
+    console.log()
+}
+
+// 3- retorne o maior valor de tres //
+
+
+
+
+// 4- defina const para que a resposta seja positive ou negative ou zero //
+const fatorRhDaAna = "+";
+
+if (fatorRhDaAna === "+") {
+    console.log ("Positive");
+} else if (fatorRhDaAna === "-") {
+    console.log ("Negative")
+} else { 
+    console.log ("Zero")
+}
+return
+
+// 5- defina 3 constantes com valores de angulos internos de um triangulo. Para validar tem 
+// que ter soma de 180g 
+
