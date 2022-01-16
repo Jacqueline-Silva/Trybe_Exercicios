4675b2d3a7021e5bd68bcf26b3338e3eccdfa0ae
Jacqueline Silva <jacque.sx@hotmail.com>
2022-01-15 22:00:12 -0300

    Exercicios do Dia 01 - Completos

--- M1_Fundamentos/bloco-07_introducao-a-js-es6-e-testes-unitarios/dia-01_js-es6_let-const-arrow-e-template/exercios.js
+++ M1_Fundamentos/bloco-07_introducao-a-js-es6-e-testes-unitarios/dia-01_js-es6_let-const-arrow-e-template/exercios.js
@@ -1,10 +1,6 @@
-/* PARTE 1 ======================================================
+// PARTE 1 ======================================================
 
-1 - Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
-    Modifique a estrutura da função para que ela seja uma arrow function .
-    Modifique as concatenações para template literals .
-*/
-
+// 1
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -18,15 +14,12 @@ const testingScope = (escopo) => {
 }
 testingScope(true);
 
-/* 
-2 -Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
-Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
-Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
-*/
+// 2
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 const ordenando = (a, b) => a - b;
 
-console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente! 
+console.log(
+  `Os números ${oddsAndEvens} se encontram ordenados de forma crescente! 
 -> ${oddsAndEvens.sort(ordenando)}`); // será necessário alterar essa linha 😉
 
 /*
@@ -36,13 +29,49 @@ REF: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Ob
 // PARTE 2 ========================================================
 
 
-//  1 - Crie uma função que receba um número e retorne seu fatorial.
+// 1 
 const fatorial = (numero) => {
   let fatorado = numero;
-  for (let index = 1; index < numero; index += 1){
-    fatorado *= index
-  } return fatorado
+  for (let index = 1; index < numero; index += 1) {
+    fatorado *= index;
+  } return fatorado;
 }; 
 console.log(fatorial(5));
 
-// 2 - Crie uma função que receba uma frase e retorne qual a maior palavra.
+/* 
+GABARITO: (RECURSIVA E OPER.TERNARIO)
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
+console.log(factorial(5));
+*/
+
+// 2 
+const maiorPalavra = (frase) => {
+  const separaParavras = frase.split(' ');
+  let guardaMaiorPalavra = '';
+  for (let index = 0; index < separaParavras.length; index +=1) {
+    if(separaParavras[index].length > guardaMaiorPalavra.length) {
+      guardaMaiorPalavra = separaParavras[index];
+    } 
+  }
+  return guardaMaiorPalavra;
+}
+console.log(maiorPalavra('João roubou pão na casa do Roberto e Maria foi fofocar para Jacqueline'));
+
+// 3 - ******* Arquivo: "exercicio3.html" ***********
+
+// 4 
+const skills = ['Lealdade', 'HTML', 'CSS', 'Escuta ativa', 'Organização'];
+
+const substitui = (string) => {
+  let original = 'Tryber x aqui';
+  let substituido = original.replace('x', string);
+
+  let resultado = 
+    `${substituido}!
+    Minhas cinco principais habilidades são:
+    ${skills.sort()}
+    #GoTrybe.`;
+  
+  return resultado;
+}
+console.log(substitui('Jacqueline'));
