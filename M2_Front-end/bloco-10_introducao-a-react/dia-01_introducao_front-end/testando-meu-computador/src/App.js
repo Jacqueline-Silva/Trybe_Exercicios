daab6e87d2496b388946adbac748ee6fdcdddb0a
Jacqueline Silva <jacque.sx@hotmail.com>
2022-02-16 08:36:33 -0300

    Adicionando B10-1 sem exercicios

---
+++ M2_Front-end/bloco-10_introducao-a-react/dia-01_introducao_front-end/testando-meu-computador/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import MyDescription from './Component.js';
+//import HelloWorld from './HelloWorld.js';
+
+
+function App() {
+  return <MyDescription />
+}
+
+export default App;
---
+++ M2_Front-end/bloco-10_introducao-a-react/dia-01_introducao_front-end/testando-meu-computador/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyDescription extends React.Component {
+  render() {
+    return ( 
+      <div>
+        <h1 className="MyDesc">Jacqueline Silva</h1>
+        <p className="MyDescP">Olá, me chamo Jacqueline e estou testando o React e coloquei a letra bem grande pois estou testando o CSS também.</p>
+      </div>
+    )
+  }
+}
+
+export default MyDescription;
