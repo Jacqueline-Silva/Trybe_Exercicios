00aa8b7436cef92929f1a03537caef6732aca777
Jacqueline Silva <jacque.sx@hotmail.com>
2022-02-16 09:30:17 -0300

    Adicionando B10-1 + exercicio 1 e 2

---
+++ M2_Front-end/bloco-10_introducao-a-react/dia-01_introducao_front-end/exercicios-react/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+
+const compromissos = ['Encontro', 'Ir a padaria', 'Ir ao mercado', 'Jogar JustDance', 'Estudar React', 'Não esquecer de comer e beber água'];
+
+
+const Task = (value) => {
+  value = compromissos;
+  let arr = value.map((e) => 
+    <li className='test'>{e}</li>)
+  return (
+    arr
+  )
+}
+
+
+function App() {
+  return (<Task/>);
+}
+
+export default App;
