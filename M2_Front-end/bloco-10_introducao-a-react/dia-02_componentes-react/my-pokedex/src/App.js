de724acabe1c94dbf1134f9b83bbad42f7b14e88
Jacqueline Silva <jacque.sx@hotmail.com>
2022-02-18 11:24:39 -0300

    Apagando arquivos desnecessarios e inserindo css

--- M2_Front-end/bloco-10_introducao-a-react/dia-02_componentes-react/my-pokedex/src/App.js
+++ M2_Front-end/bloco-10_introducao-a-react/dia-02_componentes-react/my-pokedex/src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
-import PokeInfo from './poke.js';
+import React from 'react';
+import pokemons from './data.js';
+import PokeInfo from './Poke.js';
 
-function App() {
-  return (
-    <div className="App">
-      <PokeInfo />
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className='title'> Pokedex </h1>
+        <PokeInfo pokemons={pokemons} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
