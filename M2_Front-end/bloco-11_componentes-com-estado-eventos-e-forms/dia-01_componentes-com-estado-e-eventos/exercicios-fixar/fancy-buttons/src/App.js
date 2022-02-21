cddb9d14580fecf5811dda544abd98616636cd9f
Jacqueline Silva <jacque.sx@hotmail.com>
2022-02-21 11:21:01 -0300

    ADD: Exercicio fixar - Estado e eventos

--- M2_Front-end/bloco-11_componentes-com-estado-eventos-e-forms/dia-01_componentes-com-estado-e-eventos/exercicios-fixar/fancy-buttons/src/App.js
+++ M2_Front-end/bloco-11_componentes-com-estado-eventos-e-forms/dia-01_componentes-com-estado-e-eventos/exercicios-fixar/fancy-buttons/src/App.js
@@ -6,35 +6,56 @@ class App extends React.Component {
   constructor() {
     super()
 
+    this.state = {
+      clickLeft: 0,
+      clickCenter: 0,
+      clickRight: 0
+    }
+
     this.left = this.left.bind(this);
     this.center = this.center.bind(this);
     this.right = this.right.bind(this);
   }
 
   left() {
-    console.log(this)
+    this.setState((numAnterior, _props) => ({
+      clickLeft: numAnterior.clickLeft + 1
+    }))
     console.log('Clicou no botão da esquerda')
   }
 
 
   right() {
-    console.log(this)
+    this.setState((numAnterior, _props) => ({
+      clickRight: numAnterior.clickRight + 1
+    }))
     console.log('Clicou no botão da direita')
   }
 
 
   center() {
-    console.log(this)
+    this.setState((numAnterior, _props) => ({
+      clickCenter: numAnterior.clickCenter + 1
+    }))
     console.log('Clicou no botão do centro')
   }
 
   render() { 
     return (
       <>
-        <div>
-          <button className='btn left' onClick={this.left}>Esquerda</button>
-          <button className='btn center' onClick={this.center}>Principal</button>
-          <button className='btn right' onClick={this.right}>Direita</button>
+        <div className='divPai'>
+          <div>
+            <p>{this.state.clickLeft}</p>
+            <button className='btn left' onClick={this.left}>Esquerda</button>
+          </div>
+          <div>
+            <p>{this.state.clickCenter}</p>
+            <button className='btn center' onClick={this.center}>Principal</button>
+          </div>
+          <div>
+            <p>{this.state.clickRight}</p>
+            <button className='btn right' onClick={this.right}>Direita</button>
+          </div>
         </div>
         <div>
           Olhe o console!
