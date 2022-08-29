47bf50057196870b8b557b41993c7b41554f01b1
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-29 11:10:19 -0300

    :sparkles: feat: B27/4, exercicios de fixat - incompleto

---
+++ M3_Back-end/bloco-27_programacao-orientada-a-objetos-e-solid/dia-04_solid_principios-s-o-d/exercicios-fixar/DIP/ConsoleNotification.ts
@@ -0,0 +1,10 @@
+// ConsoleNotification.ts
+import Notificator from './Notificador';
+
+export default class ConsoleNotification implements Notificator {
+  constructor(private name: string) { }
+
+  sendNotification(message: string): void {
+    console.log(`Here we go again! - ${message} from ${this.name}`);
+  }
+}
