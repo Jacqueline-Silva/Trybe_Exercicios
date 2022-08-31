de4e7daeaf27b260b4778559c4e014b709484d4d
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-31 17:18:18 -0300

    :sparkles: fix: B29/1, comentários no arquivo mongoimport.sh

--- M3_Back-end/bloco-29_introducao-ao-mongodb/dia-01_introducao-ao-MongoDB/exercicios/mongoimport.sh
+++ M3_Back-end/bloco-29_introducao-ao-mongodb/dia-01_introducao-ao-MongoDB/exercicios/mongoimport.sh
@@ -1,8 +1,8 @@
-// Dentro do diretório em que está o arquivo, copiar o json
+## Dentro do diretório em que está o arquivo, copiar o json
 docker cp books.json d6b44:/tmp/books.json
 
-// Em seguida
+## Em seguida
 docker exec d6b44 mongoimport -d book -c books --file /tmp/books.json
 
-// Retorno esperado:
-// 431 document(s) imported successfully. 0 document(s) failed to import.
+## Retorno esperado:
+## 431 document(s) imported successfully. 0 document(s) failed to import.
