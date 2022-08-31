7db7804d3a2e9c870e634404aede42bc50063975
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-31 17:12:29 -0300

    :sparkles: feat: B29/1, exercícios - 7 ao 10

---
+++ M3_Back-end/bloco-29_introducao-ao-mongodb/dia-01_introducao-ao-MongoDB/exercicios/mongoimport.sh
@@ -0,0 +1,8 @@
+// Dentro do diretório em que está o arquivo, copiar o json
+docker cp books.json d6b44:/tmp/books.json
+
+// Em seguida
+docker exec d6b44 mongoimport -d book -c books --file /tmp/books.json
+
+// Retorno esperado:
+// 431 document(s) imported successfully. 0 document(s) failed to import.
